8d492188d6a54666a5cd85d9bca3b2e25999fc91
saharvitale <113132357+saharvitale@users.noreply.github.com>
2023-01-23 14:00:34 -0500

    sinking dying

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,18 +1,106 @@
 import "../styles/style.css";
+import { menu } from "./menu";
 
-document.querySelector("#app").innerHTML = `
-  <div>
-   
+const DOMSelectors = {
+  btnA: document.querySelector(".btnA"),
+  btnB: document.querySelector(".btnB"),
+  btnC: document.querySelector(".btnC"),
+  btnAll: document.querySelector(".btnAll"),
+  display: document.querySelector(".display"),
+  him: change("ice cream"),
+  her: change("something small"),
+  them: change("cakes"),
+};
 
-    <h1>Hello Vite!!!!</h1>
+function change(change) {
+  return menu.filter((el) => el.type === `${change}`);
+}
 
-    <div class="card">
-      <button id="counter" type="button"></button>
+function remove() {
+  document.querySelectorAll(".d-card").forEach((e) => e.remove());
+  document.querySelectorAll(".subhead").forEach((e) => e.remove());
+}
+
+function subhead(type, comment) {
+  DOMSelectors.display.insertAdjacentHTML(
+    "beforebegin",
+    `<div class="subhead">
+    <h3 class="hero"> ${type}</h3>
+    <h4 class="hero"> ${comment}</h4>
+  </div>`
+  );
+}
+
+function initial() {
+  menu.forEach((el) => console.log(el));
+  subhead("all", "so indecisive!");
+  menu.forEach(function htmlher(dish) {
+    DOMSelectors.display.insertAdjacentHTML(
+      "afterbegin",
+      `<div class="d-card">
+      <h3 class="dish-name">${dish.name}</h3>
+      <h4 class="dish-description">${dish.description}</h4>
+      <h4 class="dish-name">${dish.price}</h4>
+      <img class= "image" src="${dish.img}">
+      </div>
+      `
+    );
+  });
+}
+
+function html(dish) {
+  DOMSelectors.display.insertAdjacentHTML(
+    "afterbegin",
+    `<div class="d-card">
+    <h3 class="dish-name">${dish.name}</h3>
+    <h4 class="dish-description">${dish.description}</h4>
+    <h4 class="dish-name">${dish.price}</h4>
+    <img class= "image" src="${dish.img}">
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`;
+   `
+  );
+}
 
-setupCounter(document.querySelector("#counter"));
+initial();
+//start off page with all desserts selected
+
+DOMSelectors.btnA.addEventListener("click", function () {
+  remove();
+  console.log(DOMSelectors.him);
+  subhead("ice cream", "so many different options!");
+  const map1 = Array.from(DOMSelectors.him);
+  map1.forEach((el) => html(el));
+});
+
+DOMSelectors.btnB.addEventListener("click", function () {
+  remove();
+  console.log(DOMSelectors.her);
+  subhead("something small", "the best things come in small packages");
+  const map2 = Array.from(DOMSelectors.her);
+  map2.forEach((el) => html(el));
+});
+
+DOMSelectors.btnC.addEventListener("click", function () {
+  remove();
+  console.log(DOMSelectors.them);
+  subhead("cakes", "i want them all!");
+  const map3 = Array.from(DOMSelectors.them);
+  map3.forEach((el) => html(el));
+});
+
+DOMSelectors.btnAll.addEventListener("click", function () {
+  remove();
+  initial();
+});
+//as you click each button, the subheading changes and the array w cards shown changes
+
+document.querySelector(".color").addEventListener("click", function () {
+  if (document.body.classList.contains("cool")) {
+    document.body.classList.add("warm");
+    document.body.classList.remove("cool");
+  } else {
+    document.body.classList.add("cool");
+    document.body.classList.remove("warm");
+  }
+});
+// if the body class is cool, click to change class to warm. if on warm, click to change to cool
---
+++ vite-project/js/menu.js
@@ -0,0 +1,85 @@
+const menu = [
+  {
+    type: "ice cream",
+    name: "single scoop",
+    description: "in a waffle cone with a cherry",
+    price: 3.99,
+    rating: 9.9,
+    img: "https://thebigmansworld.com/wp-content/uploads/2022/05/keto-ice-cream.jpeg",
+  },
+
+  {
+    type: "ice cream",
+    name: "triple scoop",
+    description: "choose 3 of your favorite flavors!",
+    price: 7.95,
+    rating: 9.9,
+    img: "https://media01.stockfood.com/largepreviews/NDIwMjExMg==/00135552-Triple-Scoop-Ice-Cream-Cone.jpg",
+  },
+
+  {
+    type: "ice cream",
+    name: "popsicle",
+    description: "cool fruity outside with soft cream on the inside",
+    price: 2.95,
+    rating: 9.9,
+    img: "https://i.pinimg.com/736x/f2/ee/11/f2ee11dccd504fbeae535c9450704c7b.jpg",
+  },
+
+  {
+    type: "something small",
+    name: "macaroon",
+    description: "so soft, it melts in your mouth!",
+    price: 2.5,
+    rating: 9.9,
+    img: "https://i.pinimg.com/736x/05/f5/3d/05f53d914051295e22373928b8d36ccb.jpg",
+  },
+
+  {
+    type: "something small",
+    name: "cookie",
+    description: "you can never go wrong with chocolate chip",
+    price: 3.99,
+    rating: 9.9,
+    img: "https://i.pinimg.com/736x/0b/fa/bb/0bfabb18c2ec799218f4f68cea140dd1--perfect-chocolate-chip-cookies-chocolate-chips.jpg",
+  },
+
+  {
+    type: "something small",
+    name: "donut",
+    description: "perfect with your morning coffee!",
+    price: 2.5,
+    rating: 9.9,
+    img: "https://i.pinimg.com/236x/90/70/7c/90707cb8c40914af2a0c8ac876b4f567--donuts-donuts-baked-doughnuts.jpg",
+  },
+
+  {
+    type: "cakes",
+    name: "strawberry deluxe",
+    description:
+      "strawberry filling, strawberry frosting, and strawberries on top",
+    price: 8.5,
+    rating: 9.9,
+    img: "https://i.pinimg.com/736x/f4/b4/f1/f4b4f1c3b02cb806b7a792bdbe893089.jpg",
+  },
+
+  {
+    type: "cakes",
+    name: "choco cherry",
+    description: "chocolate cake with cherry filling and topping",
+    price: 7.5,
+    rating: 9.9,
+    img: "https://i.pinimg.com/736x/c2/b2/2e/c2b22ebbbd1c11e9ed52b266b640cfa3.jpg",
+  },
+
+  {
+    type: "cakes",
+    name: "full cake",
+    description: "an entire cake of your favorite flavor",
+    price: 69.5,
+    rating: 9.9,
+    img: "https://i.pinimg.com/236x/04/06/a1/0406a1accf76130930847b6967161397.jpg",
+  },
+];
+
+export { menu };
